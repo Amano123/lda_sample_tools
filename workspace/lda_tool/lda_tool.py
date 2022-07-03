9241573e7b61b73dc0a2996d6cdba27dc53eb959
yoo <mi21a001@oecu.jp>
2022-07-04 01:23:27 +0900

    [fix]LDAの関数を整理した

--- workspace/lda_tool/lda_tool.py
+++ workspace/lda_tool/lda_tool.py
@@ -9,64 +9,26 @@ import numpy as np
 from datetime import datetime
 
 class LDA_Tool(object):
-    def __init__(self, word_list: List[List[str]]):
-        self.word_list: List[List[str]] =  word_list
-        self.dictionary: Dictionary
-        self.corpus: List[Tuple[int,int]]
-        self.lda_model: LdaModel
+    def __init__(self, dictionary, corpus, output_file_path: str):
+        self.dictionary: Dictionary = dictionary
+        self.corpus: List[Tuple[int,int]] = corpus
         self.num_topic: int = 5
-        self.tokens: List[str]
-        self.token_ids: List[int]
-        self.counter_dict: Dict[int, int]
-        self.topics_contribution: List[List[float]]
-        now = datetime.now()
-        self.output_file_path: str = \
-            f'./output/{now.strftime("%Y-%m-%d/%H-%M-%S")}'
-        os.makedirs(self.output_file_path, exist_ok=True)
 
-    def make_dictionary(self, words_list: List[List[str]]) -> Dictionary:
-        # ! corpus作成はこのクラスに必要ない？
-        """辞書を作成"""
-        dictionary = corpora.Dictionary(words_list)
-        dictionary.save(f'{self.output_file_path}/cooking.dict')
-        dictionary.save_as_text(f'{self.output_file_path}/cooking.dict.txt')
-        return dictionary
-    
-    def make_corpus(self, words_list: List[List[str]], dictionary: Dictionary) -> List[Tuple[int,int]]:
-        # ! corpus作成はこのクラスに必要ない？
-        """corpusを作成"""
-        corpus: List[Tuple[int,int]] = [
-            dictionary.doc2bow(text) for text in words_list
-            ]
-        return corpus
-    
-    def dictionary_from_tokent_id_extraction(self, dictionary: Dictionary) -> Tuple[List[str], List[int], Dict[int,int]]:
-        #! 必要ない？
-        """辞書から単語と単語IDと出現回数を抽出"""
-        # 辞書（Dictionary）から、単語とIDのペアを抽出
-        tuple_token2id: List[Tuple[str,int]] = list(dictionary.token2id.items())
-        token: List[str]
-        token_id : List[int]
-        # 転置を使って単語リスト、IDリストを作成
-        token, token_id = list(zip(*tuple_token2id))  # type: ignore
-        # 単語の出現回数を抽出
-        counter_dict: Dict[int,int] = dictionary.cfs
-        return (token, token_id, counter_dict)
-    
-    def dictionary_from_tokent_cunt_extraction(self, counter_dict: Dict[int,int]) -> List[int]:
-        #! 必要ない？
-        word_counter: List[int] = []
-        for index in range(len(counter_dict)):
-            word_counter.append(counter_dict[index])
-        return word_counter 
+        self.output_file_path: str = output_file_path
+        os.makedirs(self.output_file_path, exist_ok=True)
     
     def lda_traning(self, corpus: List[Tuple[int,int]], num_topic: int, dictionary: Dictionary) -> LdaModel:
         """LDAを学習する"""
         lda_model = LdaModel(
             corpus=corpus, 
             num_topics=num_topic, 
-            id2word=dictionary
-            )
+            id2word=dictionary,
+            alpha = 'auto',
+            eta = 'auto',
+            random_state = 0,
+            per_word_topics = True
+        )
+        lda_model.save(f"{self.output_file_path}/lda_modal")
         return lda_model
 
     def lda_predicate(self, lda_model: LdaModel) -> List[List[float]]:
@@ -85,23 +47,35 @@ class LDA_Tool(object):
         for topic_num, topic in enumerate(topics_contribution, 1):
             df_data[f"topic_{topic_num}"] = topic
         df_topics_contribution = pd.DataFrame(df_data)
-        df_topics_contribution.to_csv(f"{self.output_file_path}/test.csv")
+        df_topics_contribution.to_csv(f"{self.output_file_path}/topic.csv")
+
+    def dictionary_from_tokent_id_extraction(self, dictionary: Dictionary) -> Tuple[List[str], List[int], Dict[int,int]]:
+        """辞書から単語と単語IDと出現回数を抽出"""
+        # 辞書（Dictionary）から、単語とIDのペアを抽出
+        tuple_token2id: List[Tuple[str,int]] = list(dictionary.token2id.items())
+        token: List[str]
+        token_id : List[int]
+        # 転置を使って単語リスト、IDリストを作成
+        token, token_id = list(zip(*tuple_token2id))  # type: ignore
+        # 単語の出現回数を抽出
+        counter_dict: Dict[int,int] = dictionary.cfs
+        return (token, token_id, counter_dict)
+    
+    def dictionary_from_tokent_cunt_extraction(self, counter_dict: Dict[int,int]) -> List[int]:
+        word_counter: List[int] = []
+        for index in range(len(counter_dict)):
+            word_counter.append(counter_dict[index])
+        return word_counter
 
     def main(self):
-        # 辞書作成
-        self.dictionary = self.make_dictionary(self.word_list)
-        # 単語、単語ID、出現回数を抽出
-        (
-        self.tokens, # 単語
+        (self.tokens, # 単語
         self.token_ids, # 単語ID
         self.counter_dict # 単語の出現回数
         ) = self.dictionary_from_tokent_id_extraction(self.dictionary)
-        # 単語の回数
         self.word_counter = self.dictionary_from_tokent_cunt_extraction(self.counter_dict)
-        # corpus作成
-        self.corpus = self.make_corpus(self.word_list, self.dictionary)
         # LDAの学習開始
         self.lda_model = self.lda_traning(self.corpus, self.num_topic, self.dictionary)
         # Topicの寄与率を抽出
         self.topics_contribution = self.lda_predicate(self.lda_model)
         self.make_topic_table(self.tokens, self.token_ids, self.topics_contribution, self.word_counter)
+        return self.lda_model
